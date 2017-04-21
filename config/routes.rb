a439ca3d13c5934f866e3c736085890d229ae86c
kazumaono <flyablepoint@gmail.com>
2017-04-21 06:20:50 +0000

    ranking-want

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
-  
   resources :items, only: [:show, :new]
   resources :ownerships, only: [:create, :destroy]
+  
+  get 'rankings/want', to: 'rankings#want'
 end
